05772fdd8d815d6e2313d588f897ab41a5837d39
Kimbbakar <chowdhuryosman04@gmail.com>
2018-07-23 22:22:32 +0600

    Added view in contest Helper App

--- ContestHelper/Helper/views.py
+++ ContestHelper/Helper/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 def userprofile(request):
 
 	return render(request,"User_profile.html")
+
+
+def problembank(request):
+
+	return render(request,"problem_bank.html")
+
+
+def section(request):
+
+	return render(request,"section.html")	
+
+
+def contesthelper(request):
+
+	return render(request,"contest_helper.html")	
