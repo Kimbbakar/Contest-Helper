be3de60eab293321d93a871f37301c5a90808571
Kimbbakar <chowdhuryosman04@gmail.com>
2018-08-09 09:38:47 +0600

    Creat OJID table

--- ContestHelper/Helper/models.py
+++ ContestHelper/Helper/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class onlinejudgeid(models.Model):
+	user = models.OneToOneField(User,related_name='ojid',primary_key=True )
+	uva  = models.CharField(max_length=20);
